714fcf745213d48fab8318f46ba683e65627d8e8
KatyaItmo <chistyakova_ekaterina06@mail.ru>
2026-05-31 18:21:09 +0300

    old-old commit

---
+++ backend/src/main/java/org/example/service/PointService.java
@@ -0,0 +1,77 @@
+package org.example.service;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.example.dto.PointDTO;
+import org.example.dto.PointResponseDTO;
+import org.example.model.AreaChecker;
+import org.example.model.Point;
+import org.example.model.User;
+
+@Stateless
+public class PointService {
+	
+	@PersistenceContext(unitName = "Lab4Unit")
+    private EntityManager em;
+	
+	public List<PointResponseDTO> processPoints(List<PointDTO> dtos, String login) {
+		 User user = em.createQuery("SELECT u FROM User u WHERE u.login = :login", User.class)
+				 .setParameter("login", login)
+                 .getSingleResult();
+		 
+		 List<Point> points = new ArrayList<>();
+		 
+		 for (PointDTO dto: dtos) {
+			 long startTime = System.nanoTime();
+			 boolean hit = AreaChecker.checkHit(dto.getX(), dto.getY(), dto.getR(), dto.getClick());
+			 long endTime = System.nanoTime();
+			 
+			 Point point = new Point(dto.getX(), dto.getY(), dto.getR(), hit, user);
+			 point.setCheckTime(LocalDateTime.now());
+			 point.setExecutionTime(endTime - startTime);
+			 
+			 em.persist(point);
+			 points.add(point);
+		 }
+		 
+		 return makeResult(points);
+	}
+	
+	public List<PointResponseDTO> getHistory(String login, int page, int size) {
+		List<Point> points = em.createQuery("SELECT p FROM Point p WHERE p.user.login = :login ORDER BY p.checkTime DESC", Point.class)
+				.setParameter("login", login)
+				.setFirstResult((page - 1) * size)
+				.setMaxResults(size)
+				.getResultList();
+		
+		return makeResult(points);
+	}
+	
+	public List<PointResponseDTO> getAllUserPoints(String login) {
+		List<Point> points = em.createQuery("SELECT p FROM Point p WHERE p.user.login = :login ORDER BY p.checkTime DESC", Point.class)
+				.setParameter("login", login)
+				.getResultList();
+		
+		return makeResult(points);
+	}
+	
+	public void clearHistory(String login) {
+		em.createQuery("DELETE FROM Point p WHERE p.user.login = :login")
+				.setParameter("login", login)
+				.executeUpdate();
+	}
+	
+	private List<PointResponseDTO> makeResult(List<Point> points) {
+		return points.stream().map(p -> new PointResponseDTO(
+		        p.getX(), p.getY(), p.getR(), p.getHit(), 
+		        p.getCheckTime().toString(), p.getExecutionTime()
+		    )).collect(Collectors.toList());
+	}
+}
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../style.css';
+
+function Footer() {
+	return (
+		<footer>
+			<p className="footer-info">Дединсад @_^</p>
+		</footer>
+	);
+}
+
+export default Footer;
