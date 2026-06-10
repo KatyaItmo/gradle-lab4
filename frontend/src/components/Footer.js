6650f981beaf0524050ae8aae8bf5cb2a67f1a9a
KatyaItmo <chistyakova_ekaterina06@mail.ru>
2026-06-10 06:40:52 +0300

    5 commit

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import '../style.css';
 
 function Footer() {
+	const [vers, setVers] = useState("?");
+
+	useEffect(() => {
+		fetch('api/version')
+			.then(response => response.json())
+			.then(data => {
+				setVers(data.message);
+			})
+			.catch(error => {
+				console.error("Ошибка при получении версии:", error);
+				setVers("ошибка");
+			});
+	}, []);
+
 	return (
 		<footer>
 			<p className="footer-info">Дединсад @_^</p>
-			<p className="footer-info">Версия 1</p>
+			<p className="footer-info">Версия {vers}</p>
 		</footer>
 	);
 }
