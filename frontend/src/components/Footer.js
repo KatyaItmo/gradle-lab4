106fb7d493fbdeea81cda7e44a7953aa4895d9be
KatyaItmo <chistyakova_ekaterina06@mail.ru>
2026-06-10 05:16:13 +0300

    1 commit

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
 	return (
 		<footer>
 			<p className="footer-info">Дединсад @_^</p>
-			<p className="footer-info">Версия 3</p>
+			<p className="footer-info">Версия 1</p>
 		</footer>
 	);
 }
