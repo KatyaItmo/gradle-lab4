f28afa4d9ec5e791d4552154874cfeab492e566d
KatyaItmo <chistyakova_ekaterina06@mail.ru>
2026-06-10 11:19:39 +0300

    10 коммит

--- backend/src/main/java/org/example/rest/Version.java
+++ backend/src/main/java/org/example/rest/Version.java
@@ -12,6 +12,6 @@ public class Version {
 
     @GET
     public Response getVersion() {
-        return Response.ok("{\"message\": \"9\"}").build();
+        return Response.ok("{\"message\": \"10\"}").build();
     }
 }
