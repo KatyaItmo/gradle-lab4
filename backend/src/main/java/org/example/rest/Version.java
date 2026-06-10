40cbd515683d39d78210404e80c92ab0c9449d40
KatyaItmo <chistyakova_ekaterina06@mail.ru>
2026-06-10 06:41:27 +0300

    7 commit

--- backend/src/main/java/org/example/rest/Version.java
+++ backend/src/main/java/org/example/rest/Version.java
@@ -12,6 +12,6 @@ public class Version {
 
     @GET
     public Response getVersion() {
-        return Response.ok("{\"message\": \"6\"}").build();
+        return Response.ok("{\"message\": \"7\"}").build();
     }
 }
