2e0e6d6d1af4863707744e06f7356279531deb85
KatyaItmo <chistyakova_ekaterina06@mail.ru>
2026-06-16 01:14:07 +0300

    3.1.0

--- backend/src/main/java/org/example/rest/Version.java
+++ backend/src/main/java/org/example/rest/Version.java
@@ -12,6 +12,6 @@ public class Version {
 
     @GET
     public Response getVersion() {
-        return Response.ok("{\"message\": \"3.0.2\"}").build();
+        return Response.ok("{\"message\": \"3.1.0\"}").build();
     }
 }
